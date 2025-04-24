f18eaa9e81adcd20af21ef4697bf3da69e70e903
johnbean393 <johnbean393@gmail.com>
2025-04-24 17:38:41 +0800

    Initial Commit

---
+++ Aliyun Credit Panel/Aliyun_Credit_PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aliyun_Credit_PanelApp.swift
+//  Aliyun Credit Panel
+//
+//  Created by John Bean on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Aliyun_Credit_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aliyun Credit PanelTests/Aliyun_Credit_PanelTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aliyun_Credit_PanelTests.swift
+//  Aliyun Credit PanelTests
+//
+//  Created by John Bean on 4/24/25.
+//
+
+import Testing
+@testable import Aliyun_Credit_Panel
+
+struct Aliyun_Credit_PanelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
